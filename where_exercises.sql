2aef4ae24212a1ece16f239e990028f8720d785a
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-29 13:57:46 -0500

    Completed part 2 of where_exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,11 +1,39 @@
-# SELECT emp_no, first_name
+# SELECT COUNT(emp-no)
 # FROM employees
 # WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-# SELECT emp_no, last_name
+# SELECT COUNT(emp_no)
 # FROM employees
 # WHERE last_name LIKE 'E%'
 
-# SELECT emp_no, last_name
+# SELECT COUNT(emp_no)
 # FROM employees
 # WHERE last_name LIKE '%q%'
+
+# SELECT COUNT(emp_no)
+# FROM employees
+# WHERE first_name = 'Irena'
+#     OR first_name = 'Vidya'
+#     OR first_name = 'Maya';
+
+# SELECT COUNT(emp_no)
+# FROM employees
+# WHERE (first_name = 'Irena'
+#     OR first_name = 'Vidya'
+#     OR first_name = 'Maya')
+#     AND gender = 'M';
+
+# SELECT COUNT(emp_no)
+# FROM employees
+# WHERE last_name LIKE 'E%'
+#    OR last_name  LIKE '%E';
+
+# SELECT COUNT(emp_no)
+# FROM employees
+# WHERE last_name LIKE 'E%'
+#    AND last_name  LIKE '%E';
+
+# SELECT COUNT(emp_no)
+# FROM employees
+# WHERE last_name LIKE '%q%'
+#    AND last_name NOT LIKE '%qu%';
