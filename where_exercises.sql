0a6a1ab738e38d3f4cc389ec28ff4ddb74c1eebb
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-29 12:18:04 -0500

    Completed part 1 for where_exercises

---
+++ where_exercises.sql
@@ -0,0 +1,11 @@
+# SELECT emp_no, first_name
+# FROM employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+# SELECT emp_no, last_name
+# FROM employees
+# WHERE last_name LIKE 'E%'
+
+# SELECT emp_no, last_name
+# FROM employees
+# WHERE last_name LIKE '%q%'
