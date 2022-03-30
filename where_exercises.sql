0584c45e73d26dec303049a65ba7917abee2990b
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-30 15:02:36 -0500

    Corrected code for order_by_exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -14,27 +14,27 @@ WHERE last_name LIKE '%q%';
 SELECT emp_no, first_name
 FROM employees
 WHERE first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya';
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
 
 SELECT emp_no, first_name, gender
 FROM employees
 WHERE (first_name = 'Irena'
     OR first_name = 'Vidya'
     OR first_name = 'Maya')
-    AND gender = 'M';
+  AND gender = 'M';
 
 SELECT emp_no, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
-   OR last_name  LIKE '%E';
+   OR last_name LIKE '%E';
 
 SELECT emp_no, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
-   AND last_name  LIKE '%E';
+  AND last_name LIKE '%E';
 
 SELECT emp_no, last_name
 FROM employees
 WHERE last_name LIKE '%q%'
-   AND last_name NOT LIKE '%qu%';
+  AND last_name NOT LIKE '%qu%';
