d4da5a9e0c054018283af6f8e88630a0575e89ea
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-30 21:02:53 -0500

    Added code to functions_exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,39 +1,60 @@
-SELECT CONCAT('first_name', ' ' 'last_name') AS full_name
+use emilio;
+
+SELECT CONCAT(first_name, ' ' ,last_name) AS full_name
 FROM employees
-WHERE first_name LIKE ('%E')
+WHERE first_name LIKE ('E%')
+AND last_name LIKE('%E')
 ORDER BY emp_no DESC;
 
-# SELECT COUNT(*)
-# FROM employees
-# WHERE month(birth_date) = 12
-# AND day(birth_date) = 25;
-#
-# SELECT COUNT(*)
-# FROM employees
-# WHERE month(birth_date) = 12
-# AND day(birth_date) = 25
-# AND year(hire_date) BETWEEN 1990 and 1999
-# ORDER BY hire_date DESC
-# LIMIT 1;
-#
-# SELECT COUNT(*)
-# FROM employees
-# WHERE month(birth_date) = 12
-#   AND day(birth_date) = 25
-#   AND year(hire_date) BETWEEN 1990 and 1999
-# ORDER BY hire_date, birth_date DESC
-# LIMIT 2;
-#
+SELECT first_name, last_name
+FROM employees
+WHERE month(birth_date) = 12
+AND DAY(birth_date) = 25;
+
+SELECT first_name, last_name
+FROM employees
+WHERE YEAR(hire_date) BETWEEN 1990 AND 1999
+ AND MONTH(birth_date) = 12
+AND DAY(birth_date) = 25;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE YEAR(hire_date) BETWEEN 1990 AND 1999
+  AND MONTH(birth_date) = 12
+  AND DAY(birth_date) = 25
+ORDER BY hire_date DESC
+LIMIT 1;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE YEAR(hire_date) BETWEEN 1990 AND 1999
+  AND MONTH(birth_date) = 12
+  AND DAY(birth_date) = 25
+ORDER BY hire_date;
 
 
 
--- SELECT *
--- FROM employees
--- WHERE last_name like '%E%'
--- ORDER BY emp_no DESC;
---
---
--- SELECT *
--- FROM employees
--- WHERE first_name IN ('Irena', 'Vidya', 'Maya')
--- ORDER BY last_name DESC, first_name DESC;
+# SELECT *
+# FROM employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+# ORDER BY first_name;
+#
+# SELECT *
+# FROM employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+# ORDER BY first_name, last_name;
+#
+# SELECT *
+# FROM employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+# ORDER BY last_name, first_name;
+#
+# SELECT *
+# FROM employees
+# WHERE last_name like '%e%'
+# ORDER BY emp_no ASC;
+#
+# SELECT *
+# FROM employees
+# WHERE last_name like '%e%'
+# ORDER BY emp_no DESC;
