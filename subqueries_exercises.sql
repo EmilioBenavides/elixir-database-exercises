863eb0ca27d110351178ba25fef38435c7901c4c
Emilio Benavides <emilio.benavides68@gmail.com>
2022-04-05 17:32:01 -0500

    Completed subquieres exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,3 +7,28 @@ WHERE hire_date IN (
     FROM employees
     WHERE emp_no = 101010
     );
+
+SELECT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+    );
+
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
+
+SELECT e.first_name, e.last_name
+FROM employees e
+WHERE gender = 'F'
+AND emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > curdate()
+    );
